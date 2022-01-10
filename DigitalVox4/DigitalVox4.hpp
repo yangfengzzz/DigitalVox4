bc40a5bd71d6fde35cfc78b640b6c269072855cc
yangfengzzz <yangfengzzz@hotmail.com>
2022-01-10 17:14:45 +0800

    Initial Commit

---
+++ DigitalVox4/DigitalVox4.hpp
@@ -0,0 +1,21 @@
+//
+//  DigitalVox4.hpp
+//  DigitalVox4
+//
+//  Created by 杨丰 on 2022/1/10.
+//
+
+#ifndef DigitalVox4_
+#define DigitalVox4_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class DigitalVox4
+{
+    public:
+    void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ DigitalVox4/DigitalVox4Priv.hpp
@@ -0,0 +1,17 @@
+//
+//  DigitalVox4Priv.hpp
+//  DigitalVox4
+//
+//  Created by 杨丰 on 2022/1/10.
+//
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class DigitalVox4Priv
+{
+    public:
+    void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
