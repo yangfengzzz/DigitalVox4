cead4bd55ebe8de99942b481895a00ecc96ddc53
yangfengzzz <yangfengzzz@hotmail.com>
2022-01-20 20:51:24 +0800

    add EditorApplication

---
+++ vox.render/editor_application.cpp
@@ -0,0 +1,74 @@
+//
+//  editor_application.cpp
+//  vox.render
+//
+//  Created by 杨丰 on 2022/1/20.
+//
+
+#include "editor_application.h"
+#include "rendering/subpasses/forward_subpass.h"
+#include "engine.h"
+#include "camera.h"
+
+namespace vox {
+EditorApplication::~EditorApplication() {
+    _renderPass.reset();
+}
+
+bool EditorApplication::prepare(Engine &engine) {
+    MetalApplication::prepare(engine);
+    
+    _scene = std::make_unique<Scene>();
+    
+    auto extent = engine.window().extent();
+    loadScene(extent.width, extent.height);
+    
+    // Create a render pass descriptor for thelighting and composition pass
+    // Whatever rendered in the final pass needs to be stored so it can be displayed
+    _renderPassDescriptor.colorAttachments[0].storeAction(MTL::StoreActionStore);
+    _renderPassDescriptor.colorAttachments[0].loadAction(MTL::LoadActionClear);
+    _renderPassDescriptor.depthAttachment.loadAction(MTL::LoadActionClear);
+    _renderPassDescriptor.depthAttachment.texture(*_renderView->depthStencilTexture());
+    _renderPassDescriptor.stencilAttachment.loadAction(MTL::LoadActionClear);
+    _renderPassDescriptor.stencilAttachment.texture(*_renderView->depthStencilTexture());
+    _renderPass = std::make_unique<RenderPass>(_library, &_renderPassDescriptor);
+    _renderPass->addSubpass(std::make_unique<ForwardSubpass>(_renderView.get(), _scene.get(), _mainCamera));
+    
+    return true;
+}
+
+void EditorApplication::update(float delta_time) {
+    MetalApplication::update(delta_time);
+    _scene->update(delta_time);
+    _scene->updateShaderData(_device.get());
+
+    MTL::CommandBuffer commandBuffer = _commandQueue.commandBuffer();
+    MTL::Drawable *drawable = _renderView->currentDrawable();
+    // The final pass can only render if a drawable is available, otherwise it needs to skip
+    // rendering this frame.
+    if (drawable) {
+        // Render the lighting and composition pass
+        _renderPassDescriptor.colorAttachments[0].texture(*drawable->texture());
+        _renderPassDescriptor.depthAttachment.texture(*_renderView->depthStencilTexture());
+        _renderPassDescriptor.stencilAttachment.texture(*_renderView->depthStencilTexture());
+        _renderPass->draw(commandBuffer, "Lighting & Composition Pass");
+    }
+    // Finalize rendering here & push the command buffer to the GPU
+    commandBuffer.commit();
+    drawable->present();
+}
+
+bool EditorApplication::resize(uint32_t win_width, uint32_t win_height,
+                                uint32_t fb_width, uint32_t fb_height) {
+    MetalApplication::resize(win_width, win_height, fb_width, fb_height);
+    _scene->updateSize(win_width, win_height, fb_width, fb_height);
+    _mainCamera->resize(win_width, win_height);
+    return true;
+}
+
+void EditorApplication::inputEvent(const InputEvent &inputEvent) {
+    MetalApplication::inputEvent(inputEvent);
+    _scene->updateInputEvent(inputEvent);
+}
+
+}
---
+++ vox.render/editor_application.h
@@ -0,0 +1,56 @@
+//
+//  editor_application.hpp
+//  vox.render
+//
+//  Created by 杨丰 on 2022/1/20.
+//
+
+#ifndef editor_application_hpp
+#define editor_application_hpp
+
+#include "metal_application.h"
+
+namespace vox {
+class EditorApplication: public MetalApplication {
+public:
+    EditorApplication() = default;
+    
+    virtual ~EditorApplication();
+    
+    /**
+     * @brief Additional sample initialization
+     */
+    bool prepare(Engine &engine) override;
+    
+    /**
+     * @brief Main loop sample events
+     */
+    void update(float delta_time) override;
+        
+    bool resize(uint32_t win_width, uint32_t win_height,
+                uint32_t fb_width, uint32_t fb_height) override;
+    
+    void inputEvent(const InputEvent &inputEvent) override;
+    
+    virtual void loadScene(uint32_t width, uint32_t height) = 0;
+    
+protected:
+    Camera* _mainCamera{nullptr};
+    
+    /**
+     * @brief Holds all scene information
+     */
+    std::unique_ptr<Scene> _scene{nullptr};
+    
+private:
+    MTL::RenderPassDescriptor _renderPassDescriptor;
+    
+    /**
+     * @brief Pipeline used for rendering, it should be set up by the concrete sample
+     */
+    std::unique_ptr<RenderPass> _renderPass{nullptr};
+};
+
+}
+
+#endif /* editor_application_hpp */
