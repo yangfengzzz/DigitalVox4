a9af4dab837e93581e8413286b761ecacd1aee27
yangfengzzz <yangfengzzz@hotmail.com>
2022-01-21 15:44:32 +0800

    add fbx loader

--- vox.render/loader/fbx.cc
+++ vox.render/loader/fbx.cc
@@ -1,29 +1,8 @@
-//----------------------------------------------------------------------------//
-//                                                                            //
-// vox-animation is hosted at http://github.com/guillaumeblanc/vox-animation  //
-// and distributed under the MIT License (MIT).                               //
-//                                                                            //
-// Copyright (c) Guillaume Blanc                                              //
-//                                                                            //
-// Permission is hereby granted, free of charge, to any person obtaining a    //
-// copy of this software and associated documentation files (the "Software"), //
-// to deal in the Software without restriction, including without limitation  //
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,   //
-// and/or sell copies of the Software, and to permit persons to whom the      //
-// Software is furnished to do so, subject to the following conditions:       //
-//                                                                            //
-// The above copyright notice and this permission notice shall be included in //
-// all copies or substantial portions of the Software.                        //
-//                                                                            //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR //
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   //
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    //
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER //
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING    //
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        //
-// DEALINGS IN THE SOFTWARE.                                                  //
-//                                                                            //
-//----------------------------------------------------------------------------//
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
 
 #include "fbx.h"
 
@@ -31,7 +10,7 @@
 #include <ozz/base/maths/transform.h>
 #include <ozz/base/memory/allocator.h>
 
-namespace vox {
+namespace ozz {
 namespace loader {
 
 FbxManagerInstance::FbxManagerInstance() : fbx_manager_(nullptr) {
@@ -79,7 +58,7 @@ FbxSceneLoader::FbxSceneLoader(const char *_filename, const char *_password,
                                const FbxDefaultIOSettings &_io_settings)
 : scene_(nullptr), converter_(nullptr) {
     // Create an importer.
-    FbxImporter *importer = FbxImporter::Create(_manager, "vox file importer");
+    FbxImporter *importer = FbxImporter::Create(_manager, "ozz file importer");
     const bool initialized = importer->Initialize(_filename, -1, _io_settings);
     
     importScene(importer, initialized, _password, _manager, _io_settings);
@@ -93,7 +72,7 @@ FbxSceneLoader::FbxSceneLoader(FbxStream *_stream, const char *_password,
                                const FbxDefaultIOSettings &_io_settings)
 : scene_(nullptr), converter_(nullptr) {
     // Create an importer.
-    FbxImporter *importer = FbxImporter::Create(_manager, "vox stream importer");
+    FbxImporter *importer = FbxImporter::Create(_manager, "ozz stream importer");
     const bool initialized =
     importer->Initialize(_stream, nullptr, -1, _io_settings);
     
@@ -128,7 +107,7 @@ void FbxSceneLoader::importScene(FbxImporter *_importer,
         }
         
         // Load the scene.
-        scene_ = FbxScene::Create(_manager, "vox scene");
+        scene_ = FbxScene::Create(_manager, "ozz scene");
         bool imported = _importer->Import(scene_);
         
         if (!imported &&  // The import file may have a password
@@ -150,7 +129,7 @@ void FbxSceneLoader::importScene(FbxImporter *_importer,
         // Setup axis and system converter.
         if (imported) {
             FbxGlobalSettings &settings = scene_->GetGlobalSettings();
-            converter_ = ozz::New<FbxSystemConverter>(settings.GetAxisSystem(),
+            converter_ = New<FbxSystemConverter>(settings.GetAxisSystem(),
                                                       settings.GetSystemUnit());
         }
         
@@ -169,16 +148,16 @@ FbxSceneLoader::~FbxSceneLoader() {
     }
     
     if (converter_) {
-        ozz::Delete(converter_);
+        Delete(converter_);
         converter_ = nullptr;
     }
 }
 
 namespace {
-ozz::math::Float4x4 BuildAxisSystemMatrix(const FbxAxisSystem &_system) {
+math::Float4x4 BuildAxisSystemMatrix(const FbxAxisSystem &_system) {
     int sign;
-    ozz::math::SimdFloat4 up = ozz::math::simd_float4::y_axis();
-    ozz::math::SimdFloat4 at = ozz::math::simd_float4::z_axis();
+    math::SimdFloat4 up = math::simd_float4::y_axis();
+    math::SimdFloat4 at = math::simd_float4::z_axis();
     
     // The EUpVector specifies which axis has the up and down direction in the
     // system (typically this is the Y or Z axis). The sign of the EUpVector is
@@ -187,35 +166,35 @@ ozz::math::Float4x4 BuildAxisSystemMatrix(const FbxAxisSystem &_system) {
     const FbxAxisSystem::EUpVector eup = _system.GetUpVector(sign);
     switch (eup) {
         case FbxAxisSystem::eXAxis: {
-            up = ozz::math::simd_float4::Load(1.f * sign, 0.f, 0.f, 0.f);
+            up = math::simd_float4::Load(1.f * sign, 0.f, 0.f, 0.f);
             // If the up axis is X, the remain two axes will be Y And Z, so the
             // ParityEven is Y, and the ParityOdd is Z
             if (_system.GetFrontVector(sign) == FbxAxisSystem::eParityEven) {
-                at = ozz::math::simd_float4::Load(0.f, 1.f * sign, 0.f, 0.f);
+                at = math::simd_float4::Load(0.f, 1.f * sign, 0.f, 0.f);
             } else {
-                at = ozz::math::simd_float4::Load(0.f, 0.f, 1.f * sign, 0.f);
+                at = math::simd_float4::Load(0.f, 0.f, 1.f * sign, 0.f);
             }
             break;
         }
         case FbxAxisSystem::eYAxis: {
-            up = ozz::math::simd_float4::Load(0.f, 1.f * sign, 0.f, 0.f);
+            up = math::simd_float4::Load(0.f, 1.f * sign, 0.f, 0.f);
             // If the up axis is Y, the remain two axes will X And Z, so the
             // ParityEven is X, and the ParityOdd is Z
             if (_system.GetFrontVector(sign) == FbxAxisSystem::eParityEven) {
-                at = ozz::math::simd_float4::Load(1.f * sign, 0.f, 0.f, 0.f);
+                at = math::simd_float4::Load(1.f * sign, 0.f, 0.f, 0.f);
             } else {
-                at = ozz::math::simd_float4::Load(0.f, 0.f, 1.f * sign, 0.f);
+                at = math::simd_float4::Load(0.f, 0.f, 1.f * sign, 0.f);
             }
             break;
         }
         case FbxAxisSystem::eZAxis: {
-            up = ozz::math::simd_float4::Load(0.f, 0.f, 1.f * sign, 0.f);
+            up = math::simd_float4::Load(0.f, 0.f, 1.f * sign, 0.f);
             // If the up axis is Z, the remain two axes will X And Y, so the
             // ParityEven is X, and the ParityOdd is Y
             if (_system.GetFrontVector(sign) == FbxAxisSystem::eParityEven) {
-                at = ozz::math::simd_float4::Load(1.f * sign, 0.f, 0.f, 0.f);
+                at = math::simd_float4::Load(1.f * sign, 0.f, 0.f, 0.f);
             } else {
-                at = ozz::math::simd_float4::Load(0.f, 1.f * sign, 0.f, 0.f);
+                at = math::simd_float4::Load(0.f, 1.f * sign, 0.f, 0.f);
             }
             break;
         }
@@ -230,15 +209,15 @@ ozz::math::Float4x4 BuildAxisSystemMatrix(const FbxAxisSystem &_system) {
     // parameter to determine the direction of the third axis just as the
     // EUpVector sign. It determines if the axis system is right-handed or
     // left-handed just as the enum values.
-    ozz::math::SimdFloat4 right;
+    math::SimdFloat4 right;
     if (_system.GetCoorSystem() == FbxAxisSystem::eRightHanded) {
-        right = ozz::math::Cross3(up, at);
+        right = math::Cross3(up, at);
     } else {
-        right = ozz::math::Cross3(at, up);
+        right = math::Cross3(at, up);
     }
     
-    const ozz::math::Float4x4 matrix = {
-        {right, up, at, ozz::math::simd_float4::w_axis()}};
+    const math::Float4x4 matrix = {
+        {right, up, at, math::simd_float4::w_axis()}};
     
     return matrix;
 }
@@ -247,7 +226,7 @@ ozz::math::Float4x4 BuildAxisSystemMatrix(const FbxAxisSystem &_system) {
 FbxSystemConverter::FbxSystemConverter(const FbxAxisSystem &_from_axis,
                                        const FbxSystemUnit &_from_unit) {
     // Build axis system conversion matrix.
-    const ozz::math::Float4x4 from_matrix = BuildAxisSystemMatrix(_from_axis);
+    const math::Float4x4 from_matrix = BuildAxisSystemMatrix(_from_axis);
     
     // Finds unit conversion ratio to meters, where GetScaleFactor() is in cm.
     const float to_meters =
@@ -255,62 +234,62 @@ FbxSystemConverter::FbxSystemConverter(const FbxAxisSystem &_from_axis,
     
     // Builds conversion matrices.
     convert_ = Invert(from_matrix) *
-    ozz::math::Float4x4::Scaling(ozz::math::simd_float4::Load1(to_meters));
+    math::Float4x4::Scaling(math::simd_float4::Load1(to_meters));
     inverse_convert_ = Invert(convert_);
     inverse_transpose_convert_ = Transpose(inverse_convert_);
 }
 
-ozz::math::Float4x4 FbxSystemConverter::convertMatrix(const FbxAMatrix &_m) const {
-    const ozz::math::Float4x4 m = {{
-        ozz::math::simd_float4::Load(static_cast<float>(_m[0][0]), static_cast<float>(_m[0][1]),
+math::Float4x4 FbxSystemConverter::convertMatrix(const FbxAMatrix &_m) const {
+    const math::Float4x4 m = {{
+        math::simd_float4::Load(static_cast<float>(_m[0][0]), static_cast<float>(_m[0][1]),
                                      static_cast<float>(_m[0][2]), static_cast<float>(_m[0][3])),
-        ozz::math::simd_float4::Load(static_cast<float>(_m[1][0]), static_cast<float>(_m[1][1]),
+        math::simd_float4::Load(static_cast<float>(_m[1][0]), static_cast<float>(_m[1][1]),
                                      static_cast<float>(_m[1][2]), static_cast<float>(_m[1][3])),
-        ozz::math::simd_float4::Load(static_cast<float>(_m[2][0]), static_cast<float>(_m[2][1]),
+        math::simd_float4::Load(static_cast<float>(_m[2][0]), static_cast<float>(_m[2][1]),
                                      static_cast<float>(_m[2][2]), static_cast<float>(_m[2][3])),
-        ozz::math::simd_float4::Load(static_cast<float>(_m[3][0]), static_cast<float>(_m[3][1]),
+        math::simd_float4::Load(static_cast<float>(_m[3][0]), static_cast<float>(_m[3][1]),
                                      static_cast<float>(_m[3][2]), static_cast<float>(_m[3][3])),
     }};
     return convert_ * m * inverse_convert_;
 }
 
-ozz::math::Float3 FbxSystemConverter::convertPoint(const FbxVector4 &_p) const {
-    const ozz::math::SimdFloat4 p_in = ozz::math::simd_float4::Load(static_cast<float>(_p[0]), static_cast<float>(_p[1]),
+math::Float3 FbxSystemConverter::convertPoint(const FbxVector4 &_p) const {
+    const math::SimdFloat4 p_in = math::simd_float4::Load(static_cast<float>(_p[0]), static_cast<float>(_p[1]),
                                                                     static_cast<float>(_p[2]), 1.f);
-    const ozz::math::SimdFloat4 p_out = convert_ * p_in;
-    ozz::math::Float3 ret;
-    ozz::math::Store3PtrU(p_out, &ret.x);
+    const math::SimdFloat4 p_out = convert_ * p_in;
+    math::Float3 ret;
+    math::Store3PtrU(p_out, &ret.x);
     return ret;
 }
 
-ozz::math::Float3 FbxSystemConverter::convertVector(const FbxVector4 &_p) const {
-    const ozz::math::SimdFloat4 p_in = ozz::math::simd_float4::Load(static_cast<float>(_p[0]), static_cast<float>(_p[1]),
+math::Float3 FbxSystemConverter::convertVector(const FbxVector4 &_p) const {
+    const math::SimdFloat4 p_in = math::simd_float4::Load(static_cast<float>(_p[0]), static_cast<float>(_p[1]),
                                                                     static_cast<float>(_p[2]), 0.f);
-    const ozz::math::SimdFloat4 p_out = inverse_transpose_convert_ * p_in;
-    ozz::math::Float3 ret;
-    ozz::math::Store3PtrU(p_out, &ret.x);
+    const math::SimdFloat4 p_out = inverse_transpose_convert_ * p_in;
+    math::Float3 ret;
+    math::Store3PtrU(p_out, &ret.x);
     return ret;
 }
 
 bool FbxSystemConverter::convertTransform(const FbxAMatrix &_m,
-                                          ozz::math::Transform *_transform) const {
+                                          math::Transform *_transform) const {
     assert(_transform);
     
-    const ozz::math::Float4x4 matrix = convertMatrix(_m);
+    const math::Float4x4 matrix = convertMatrix(_m);
     
-    ozz::math::SimdFloat4 translation, rotation, scale;
+    math::SimdFloat4 translation, rotation, scale;
     if (ToAffine(matrix, &translation, &rotation, &scale)) {
-        ozz::math::Transform transform;
-        ozz::math::Store3PtrU(translation, &_transform->translation.x);
-        ozz::math::StorePtrU(ozz::math::Normalize4(rotation), &_transform->rotation.x);
-        ozz::math::Store3PtrU(scale, &_transform->scale.x);
+        math::Transform transform;
+        math::Store3PtrU(translation, &_transform->translation.x);
+        math::StorePtrU(math::Normalize4(rotation), &_transform->rotation.x);
+        math::Store3PtrU(scale, &_transform->scale.x);
         return true;
     }
     
     // Failed to decompose matrix, reset transform to identity.
-    *_transform = ozz::math::Transform::identity();
+    *_transform = math::Transform::identity();
     return false;
 }
 
 }  // namespace loader
-}  // namespace vox
+}  // namespace ozz
--- vox.render/loader/fbx.h
+++ vox.render/loader/fbx.h
@@ -1,39 +1,18 @@
-//----------------------------------------------------------------------------//
-//                                                                            //
-// vox-animation is hosted at http://github.com/guillaumeblanc/vox-animation  //
-// and distributed under the MIT License (MIT).                               //
-//                                                                            //
-// Copyright (c) Guillaume Blanc                                              //
-//                                                                            //
-// Permission is hereby granted, free of charge, to any person obtaining a    //
-// copy of this software and associated documentation files (the "Software"), //
-// to deal in the Software without restriction, including without limitation  //
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,   //
-// and/or sell copies of the Software, and to permit persons to whom the      //
-// Software is furnished to do so, subject to the following conditions:       //
-//                                                                            //
-// The above copyright notice and this permission notice shall be included in //
-// all copies or substantial portions of the Software.                        //
-//                                                                            //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR //
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   //
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    //
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER //
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING    //
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        //
-// DEALINGS IN THE SOFTWARE.                                                  //
-//                                                                            //
-//----------------------------------------------------------------------------//
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
 
-#ifndef VOX_VOX_ANIMATION_OFFLINE_FBX_FBX_H_
-#define VOX_VOX_ANIMATION_OFFLINE_FBX_FBX_H_
+#ifndef OZZ_OZZ_ANIMATION_OFFLINE_FBX_FBX_H_
+#define OZZ_OZZ_ANIMATION_OFFLINE_FBX_FBX_H_
 
 #include <fbxsdk.h>
 
 #include <ozz/base/maths/simd_math.h>
 #include <ozz/base/maths/transform.h>
 
-namespace vox {
+namespace ozz {
 namespace loader {
 
 // Manages FbxManager instance.
@@ -90,7 +69,7 @@ public:
 };
 
 // Implements axis system and unit system conversion helper, from any Fbx system
-// to vox system (FbxAxisSystem::eYAxis, FbxAxisSystem::eParityOdd,
+// to ozz system (FbxAxisSystem::eYAxis, FbxAxisSystem::eParityOdd,
 // FbxAxisSystem::eRightHanded, meter).
 // While Fbx sdk FbxAxisSystem::ConvertScene and FbxSystem::ConvertScene only
 // affect scene root, this class functions can be used to bake nodes, vertices,
@@ -101,36 +80,36 @@ public:
     FbxSystemConverter(const FbxAxisSystem &_from_axis,
                        const FbxSystemUnit &_from_unit);
     
-    // Converts a fbx matrix to an vox Float4x4 matrix, in vox axis and unit
+    // Converts a fbx matrix to an ozz Float4x4 matrix, in ozz axis and unit
     // systems, using _m' = C * _m * (C-1) operation.
-    ozz::math::Float4x4 convertMatrix(const FbxAMatrix &_m) const;
+    math::Float4x4 convertMatrix(const FbxAMatrix &_m) const;
     
-    // Converts fbx matrix to an vox transform, in vox axis and unit systems,
+    // Converts fbx matrix to an ozz transform, in ozz axis and unit systems,
     // using _m' = C * _m * (C-1) operation.
     // Can return false if matrix isn't affine.
     bool convertTransform(const FbxAMatrix &_m,
-                          ozz::math::Transform *_transform) const;
+                          math::Transform *_transform) const;
     
-    // Converts fbx FbxVector4 point to an vox Float3, in vox axis and unit
+    // Converts fbx FbxVector4 point to an ozz Float3, in ozz axis and unit
     // systems, using _p' = C * _p operation.
-    ozz::math::Float3 convertPoint(const FbxVector4 &_p) const;
+    math::Float3 convertPoint(const FbxVector4 &_p) const;
     
-    // Converts fbx FbxVector4 vector to an vox Float3, in vox axis and unit
+    // Converts fbx FbxVector4 vector to an ozz Float3, in ozz axis and unit
     // systems, using _p' = ((C-1)-T) * _p operation. Normals are converted
     // using the inverse transpose matrix to support non-uniform scale
     // transformations.
-    ozz::math::Float3 convertVector(const FbxVector4 &_p) const;
+    math::Float3 convertVector(const FbxVector4 &_p) const;
     
 private:
-    // The matrix used to convert from "from" axis/unit to vox coordinate system
+    // The matrix used to convert from "from" axis/unit to ozz coordinate system
     // base.
-    ozz::math::Float4x4 convert_;
+    math::Float4x4 convert_;
     
     // The inverse of convert_ matrix.
-    ozz::math::Float4x4 inverse_convert_;
+    math::Float4x4 inverse_convert_;
     
     // The transpose inverse of convert_ matrix.
-    ozz::math::Float4x4 inverse_transpose_convert_;
+    math::Float4x4 inverse_transpose_convert_;
 };
 
 // Loads a scene from a Fbx file.
@@ -170,5 +149,5 @@ private:
 };
 
 }  // namespace loader
-}  // namespace vox
-#endif  // VOX_VOX_ANIMATION_OFFLINE_FBX_FBX_H_
+}  // namespace ozz
+#endif  // OZZ_OZZ_ANIMATION_OFFLINE_FBX_FBX_H_
