22b6111f070eae573f564b30c2fa15cd53d33c40
yangfengzzz <yangfengzzz@hotmail.com>
2022-01-21 10:22:53 +0800

    add editor target

---
+++ editor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  editor
+//
+//  Created by 杨丰 on 2022/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
