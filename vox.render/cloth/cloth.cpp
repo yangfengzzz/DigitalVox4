ff1515823904042bd64707c3792488f9f5596716
yangfengzzz <yangfengzzz@hotmail.com>
2022-02-17 11:31:13 +0800

    add nvcloth extension into proj

--- vox.render/cloth/cloth.cpp
+++ vox.render/cloth/cloth.cpp
@@ -7,6 +7,8 @@
 
 #include "cloth.h"
 #include <NvCloth/Factory.h>
+#include <NvCloth/Solver.h>
+#include <NvClothExt/ClothFabricCooker.h>
 
 namespace vox {
 void Cloth::init() {
@@ -15,6 +17,18 @@ void Cloth::init() {
         //error
     }
     
+    nv::cloth::ClothMeshDesc meshDesc;
+    //Fill meshDesc with data
+    meshDesc.setToDefault();
+    physx::PxVec3 gravity(0.0f, -9.8f, 0.0f);
+    nv::cloth::Vector<int32_t>::Type phaseTypeInfo;
+    nv::cloth::Fabric* fabric = NvClothCookFabricFromMesh(factory, meshDesc, gravity, &phaseTypeInfo);
+    
+    nv::cloth::Cloth* cloth = factory->createCloth(nv::cloth::Range<physx::PxVec4>(NULL, NULL), *fabric);
+    
+    nv::cloth::Solver* solver = factory->createSolver();
+    solver->addCloth(cloth);
+    solver->endSimulation();
 }
 
 }
