4ad6667c3c5994d99e2087a578a971b66548e226
yangfengzzz <yangfengzzz@hotmail.com>
2022-02-18 09:57:46 +0800

    add PlaneCollisionApp

--- apps/cloth/cloth_apps.h
+++ apps/cloth/cloth_apps.h
@@ -9,12 +9,14 @@
 
 #include "capsule_app.h" // capsule collider
 #include "ccd_app.h" // capsule collider with animation ccd
+#include "plane_collision_app.h" // plane collider
 #include "free_fall_app.h" // plane collider
 #include "friction_app.h" // friction comparsion
 #include "convex_collision_app.h" // convex collider
 #include "distance_constraint_app.h" // distance constraints
 #include "geodesic_app.h" // geodesic distance for tether constraints
 #include "inter_collision_app.h" // lap together
+
 #include "multi_solver_app.h"
 #include "local_global_app.h"
 
--- apps/main.cpp
+++ apps/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char * argv[]) {
         
     auto code = engine.initialize();
     if (code == vox::ExitCode::Success) {
-        engine.setApp(std::make_unique<vox::cloth::MultiSolverApp>());
+        engine.setApp(std::make_unique<vox::cloth::PlaneCollisionApp>());
         code = engine.mainLoop();
     }
     
