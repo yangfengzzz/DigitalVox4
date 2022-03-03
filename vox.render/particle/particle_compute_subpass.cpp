cff9e6730e976f887fb648f067a6dc3e99fc4f06
yangfengzzz <yangfengzzz@hotmail.com>
2022-03-03 18:04:53 +0800

    add pipeline into ParticleComputeSubpass

--- vox.render/particle/particle_compute_subpass.cpp
+++ vox.render/particle/particle_compute_subpass.cpp
@@ -5,6 +5,9 @@
 //  property of any third parties.
 
 #include "particle_compute_subpass.h"
+#include "rendering/render_pass.h"
+#include "metal_helpers.h"
+#include <glog/logging.h>
 
 namespace vox {
 ParticleComputeSubpass::ParticleComputeSubpass(RenderContext *context,
@@ -15,14 +18,109 @@ ComputeSubpass(context, scene, camera){
 }
 
 void ParticleComputeSubpass::prepare() {
+    NS::Error* error;
+    // emission
+    {
+        _emissionCompute =
+        CLONE_METAL_CUSTOM_DELETER(MTL::Function,
+                                   _pass->library().newFunction(NS::String::string("particle_emission",
+                                                                                   NS::StringEncoding::UTF8StringEncoding)));
+        _emissionState = CLONE_METAL_CUSTOM_DELETER(MTL::ComputePipelineState,
+                                                    _context->device().newComputePipelineState(_emissionCompute.get(), &error));
+        if (error != nullptr) {
+            LOG(ERROR) << "Error: could not load Metal shader: "
+            << error->description()->cString(NS::StringEncoding::UTF8StringEncoding) << std::endl;
+        }
+    }
     
+    // simulation
+    {
+        _simulationCompute =
+        CLONE_METAL_CUSTOM_DELETER(MTL::Function,
+                                   _pass->library().newFunction(NS::String::string("particle_simulation",
+                                                                                   NS::StringEncoding::UTF8StringEncoding)));
+        _simulationState = CLONE_METAL_CUSTOM_DELETER(MTL::ComputePipelineState,
+                                                      _context->device().newComputePipelineState(_simulationCompute.get(), &error));
+        if (error != nullptr) {
+            LOG(ERROR) << "Error: could not load Metal shader: "
+            << error->description()->cString(NS::StringEncoding::UTF8StringEncoding) << std::endl;
+        }
+    }
+    
+    // fillIndices
+    {
+        _fillIndicesCompute =
+        CLONE_METAL_CUSTOM_DELETER(MTL::Function,
+                                   _pass->library().newFunction(NS::String::string("particle_fill_indices",
+                                                                                   NS::StringEncoding::UTF8StringEncoding)));
+        _fillIndicesState = CLONE_METAL_CUSTOM_DELETER(MTL::ComputePipelineState,
+                                                       _context->device().newComputePipelineState(_fillIndicesCompute.get(), &error));
+        if (error != nullptr) {
+            LOG(ERROR) << "Error: could not load Metal shader: "
+            << error->description()->cString(NS::StringEncoding::UTF8StringEncoding) << std::endl;
+        }
+    }
+    
+    // calculate dp
+    {
+        _calculateDPCompute =
+        CLONE_METAL_CUSTOM_DELETER(MTL::Function,
+                                   _pass->library().newFunction(NS::String::string("particle_calculate_dp",
+                                                                                   NS::StringEncoding::UTF8StringEncoding)));
+        _calculateDPState = CLONE_METAL_CUSTOM_DELETER(MTL::ComputePipelineState,
+                                                       _context->device().newComputePipelineState(_calculateDPCompute.get(), &error));
+        if (error != nullptr) {
+            LOG(ERROR) << "Error: could not load Metal shader: "
+            << error->description()->cString(NS::StringEncoding::UTF8StringEncoding) << std::endl;
+        }
+    }
+    
+    // sort step
+    {
+        _sortStepCompute =
+        CLONE_METAL_CUSTOM_DELETER(MTL::Function,
+                                   _pass->library().newFunction(NS::String::string("particle_sort_step",
+                                                                                   NS::StringEncoding::UTF8StringEncoding)));
+        _sortStepState = CLONE_METAL_CUSTOM_DELETER(MTL::ComputePipelineState,
+                                                    _context->device().newComputePipelineState(_sortStepCompute.get(), &error));
+        if (error != nullptr) {
+            LOG(ERROR) << "Error: could not load Metal shader: "
+            << error->description()->cString(NS::StringEncoding::UTF8StringEncoding) << std::endl;
+        }
+    }
+    
+    // sort final
+    {
+        _sortFinalCompute =
+        CLONE_METAL_CUSTOM_DELETER(MTL::Function,
+                                   _pass->library().newFunction(NS::String::string("particle_sort_final",
+                                                                                   NS::StringEncoding::UTF8StringEncoding)));
+        _sortFinalState = CLONE_METAL_CUSTOM_DELETER(MTL::ComputePipelineState,
+                                                     _context->device().newComputePipelineState(_sortFinalCompute.get(), &error));
+        if (error != nullptr) {
+            LOG(ERROR) << "Error: could not load Metal shader: "
+            << error->description()->cString(NS::StringEncoding::UTF8StringEncoding) << std::endl;
+        }
+    }
 }
 
 void ParticleComputeSubpass::compute(MTL::ComputeCommandEncoder &commandEncoder) {
     auto particles = _scene->_particleManager.particles();
     for (auto& particle : particles) {
-        commandEncoder.setBytes(&particle->timeStep(), sizeof(float), 0);
+        _computeSingle(particle, commandEncoder);
     }
 }
+
+void ParticleComputeSubpass::_computeSingle(Particle* particle, MTL::ComputeCommandEncoder &commandEncoder) {
+    commandEncoder.setBytes(&particle->timeStep(), sizeof(float), 0);
+    
+    commandEncoder.setComputePipelineState(_emissionState.get());
+    commandEncoder.setComputePipelineState(_simulationState.get());
+    commandEncoder.setComputePipelineState(_fillIndicesState.get());
+    commandEncoder.setComputePipelineState(_calculateDPState.get());
+    commandEncoder.setComputePipelineState(_sortStepState.get());
+    commandEncoder.setComputePipelineState(_sortFinalState.get());
+}
+
 
 }
--- vox.render/particle/particle_compute_subpass.h
+++ vox.render/particle/particle_compute_subpass.h
@@ -10,6 +10,8 @@
 #include "rendering/compute_subpass.h"
 
 namespace vox {
+class Particle;
+
 class ParticleComputeSubpass : public ComputeSubpass {
 public:
     ParticleComputeSubpass(RenderContext *context,
@@ -21,7 +23,25 @@ public:
     void compute(MTL::ComputeCommandEncoder &commandEncoder) override;
 
 private:
+    void _computeSingle(Particle* particle, MTL::ComputeCommandEncoder &commandEncoder);
     
+    std::shared_ptr<MTL::Function> _emissionCompute{nullptr};
+    std::shared_ptr<MTL::ComputePipelineState> _emissionState{nullptr};
+    
+    std::shared_ptr<MTL::Function> _simulationCompute{nullptr};
+    std::shared_ptr<MTL::ComputePipelineState> _simulationState{nullptr};
+    
+    std::shared_ptr<MTL::Function> _fillIndicesCompute{nullptr};
+    std::shared_ptr<MTL::ComputePipelineState> _fillIndicesState{nullptr};
+    
+    std::shared_ptr<MTL::Function> _calculateDPCompute{nullptr};
+    std::shared_ptr<MTL::ComputePipelineState> _calculateDPState{nullptr};
+    
+    std::shared_ptr<MTL::Function> _sortStepCompute{nullptr};
+    std::shared_ptr<MTL::ComputePipelineState> _sortStepState{nullptr};
+    
+    std::shared_ptr<MTL::Function> _sortFinalCompute{nullptr};
+    std::shared_ptr<MTL::ComputePipelineState> _sortFinalState{nullptr};
 };
 }
 
